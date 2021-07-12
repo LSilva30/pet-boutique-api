04b6320def63f1cd0547dfd005e721dffb78d041
luizsilva <luizfsilva.091595@gmail.com>
2021-07-12 17:04:46 -0400

    added a create customer

--- src/customers.js
+++ src/customers.js
@@ -12,52 +12,66 @@ function connectDb() {
 }
 
 exports.getCustomers = (request, response) => {
-    const db = connectDb()
-    db.collection('customers').get()
+  const db = connectDb()
+  db.collection('customers')
+    .get()
     .then(customerCollection => {
-        const allCustomers = customerCollection.docs.map(doc => doc.data())
-        response.send(allCustomers)
+      const allCustomers = customerCollection.docs.map(doc => doc.data())
+      response.send(allCustomers)
     })
     .catch(err => {
-        console.error(err)
-        response.status(500).send(err)
+      console.error(err)
+      response.status(500).send(err)
     })
 }
 
-
-exports.getCustomerById = ( request, response ) => {
-    if(!request.params.customerId) {
-        response.status(400).send('No Customer Specified!')
-        return
-    }
-    const db = connectDb()
-    db.collection('customers').doc(request.params.customerId).get()
-        .then(customerDoc => {
-            const customer = customerDoc.data()
-            customer.id = customerDoc.id
-            response.send(customer)
+exports.getCustomerById = (request, response) => {
+  if (!request.params.customerId) {
+    response.status(400).send('No Customer Specified!')
+    return
+  }
+  const db = connectDb()
+  db.collection('customers')
+    .doc(request.params.customerId)
+    .get()
+    .then(customerDoc => {
+      const customer = customerDoc.data()
+      customer.id = customerDoc.id
+      response.send(customer)
     })
-        .catch( err => {
-        console.error(err)
-        response.status(500).send(err)
+    .catch(err => {
+      console.error(err)
+      response.status(500).send(err)
     })
 }
 
-exports.getCustomerByQuery = ( req, res ) => {
-    // get query from req.query
-    const { fname } = req.query
-    //connect to firestore
-    const db = connectDb()
-    // search customers collection based on query
-    db.collection('customers').where('firstName', '==', fname).get()
-     //respond with results
+exports.getCustomerByQuery = (req, res) => {
+  // get query from req.query
+  const { fname } = req.query
+  //connect to firestore
+  const db = connectDb()
+  // search customers collection based on query
+  db.collection('customers')
+    .where('firstName', '==', fname)
+    .get()
+    //respond with results
     .then(customerCollection => {
-        const matches = customerCollection.docs.map(doc => {
-            let customer = doc.data()
-            customer.id = doc.id
-            return customer
-        })
-        res.send(matches)
+      const matches = customerCollection.docs.map(doc => {
+        let customer = doc.data()
+        customer.id = doc.id
+        return customer
+      })
+      res.send(matches)
     })
-    .catch( err => res.status(500).send(err))
+    .catch(err => res.status(500).send(err))
+}
+
+exports.createCustomer = (req, res) => {
+  const db = connectDb()
+
+
+  db.collection('customers')
+    .add(req.body)
+    .then(docRef => res.send(docRef))
+    .catch(err => res.status(500).send('Customer could not be created'))
 }
